aca47111f4846fae66c9b981c26e56b1eda01db0
João Vitor Ferreira Ribeiro <joaovitorpvh777@gmail.com>
2026-06-28 23:08:52 -0400

    CHORE: Metódos no service com DTO para o CRUD

--- src/main/java/dev/joaoVitor/AdvinhadorDePessoas/service/AdvinhaService.java
+++ src/main/java/dev/joaoVitor/AdvinhadorDePessoas/service/AdvinhaService.java
@@ -6,6 +6,10 @@ import dev.joaoVitor.AdvinhadorDePessoas.model.AdvinhaModel;
 import dev.joaoVitor.AdvinhadorDePessoas.repository.AdvinhaRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class AdvinhaService {
 
@@ -22,5 +26,37 @@ public class AdvinhaService {
         advinha = advinhaRepository.save(advinha);
         return advinhaMapper.map(advinha);
     }
+
+    public List<AdvinhaDTO> listar(){
+        List<AdvinhaModel> lista = advinhaRepository.findAll();
+        return lista.stream().map(advinhaMapper::map).collect(Collectors.toList());
+    }
+
+    public AdvinhaDTO listarPorId(Long id){
+        Optional<AdvinhaModel> itemId = advinhaRepository.findById(id);
+        return itemId.map(advinhaMapper::map).orElse(null);
+    }
+
+    public AdvinhaDTO atualizar(AdvinhaDTO advinhaDTO, Long id){
+        Optional<AdvinhaModel> item_existe = advinhaRepository.findById(id);
+        if(item_existe.isPresent()){
+            AdvinhaModel item_atualizado = advinhaMapper.map(advinhaDTO);
+            item_atualizado.setId(id);
+            item_atualizado = advinhaRepository.save(item_atualizado);
+            return advinhaMapper.map(item_atualizado);
+        }
+        return null;
+    }
+
+    public String deletar(Long id) {
+       Optional<AdvinhaModel> item = advinhaRepository.findById(id);
+       if(item.isPresent()){
+           advinhaRepository.deleteById(id);
+           return "O item foi deletado";
+       }
+
+       return null;
+    }
+
 
 }
