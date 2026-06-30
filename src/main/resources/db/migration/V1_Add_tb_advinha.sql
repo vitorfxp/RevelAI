0289cf229e1e60e6ac18650b65f3dd0734077db8
João Vitor Ferreira Ribeiro <joaovitorpvh777@gmail.com>
2026-06-30 01:16:12 -0400

    CHORE: Configuração do flyway + H2 com persistência de dados

---
+++ src/main/resources/db/migration/V1_Add_tb_advinha.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tb_advinha (
+                            id BIGINT PRIMARY KEY,
+                            descricao VARCHAR(255),
+                            altura INT,
+                            peso INT,
+                            idade INT,
+                            caracteristicas VARCHAR(255),
+                            nacionalidade VARCHAR(255)
+);
